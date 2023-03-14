604850f12bb87e99d9c8b3099a77347b45af614f
jancys <duchola1@gmail.com>
2023-03-14 15:33:35 +0100

    Add drawLine to UI

--- src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DriverToDrawer.java
+++ src/main/java/edu/kis/powp/jobs2d/drivers/adapter/DriverToDrawer.java
@@ -4,6 +4,7 @@ import edu.kis.legacy.drawer.panel.DrawPanelController;
 import edu.kis.legacy.drawer.shape.ILine;
 import edu.kis.legacy.drawer.shape.LineFactory;
 import edu.kis.powp.jobs2d.Job2dDriver;
+import edu.kis.powp.jobs2d.features.DrawerFeature;
 import edu.kis.powp.jobs2d.features.DriverFeature;
 
 import java.sql.Driver;
@@ -30,7 +31,7 @@ public class DriverToDrawer extends DrawPanelController implements Job2dDriver {
 		line.setStartCoordinates(this.startX, this.startY);
 		line.setEndCoordinates(x, y);
 		setPosition(x,y);
-
+		DrawerFeature.getDrawerController().drawLine(line);
 		drawLine(line);
 	}
 
